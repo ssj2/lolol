a0fdb827db02172bd50327c9781d64846553564d
ssj2 <mstasiak.94@wp.pl>
2015-06-18 13:56:21 +0200

    Update plik.py

--- plik.py
+++ plik.py
@@ -1,20 +1,21 @@
 import json
 
-links = (("Jhon","Rob"),("Rick","Henry"),("Jack","Mark"),("Poul","Chris"),("Nick","George"),("Bob","Sam"),("Theon","Jay"),("Tom","Ronald"))
+names = (("Jhon","Rob"),("Rick","Henry"),("Jack","Mark"),("Poul","Chris"),("Nick","George"),("Bob","Sam"),("Theon","Jay"),("Tom","Ronald"))
 
 name_to_node = {}
-root = {'name': 'Root', 'children': []}
+lol = {'name': 'Lol', 'children': []}
 
-for parent, child in links:
+for parent, child in names:
 
     parent_node = name_to_node.get(parent)
 
     if not parent_node:
+
         name_to_node[parent] = parent_node = {'name': parent}
-        root['children'].append(parent_node)
+        lol['children'].append(parent_node)
     name_to_node[child] = child_node = {'name': child}
     parent_node.setdefault('children', []).append(child_node)
 
-print json.dumps(root, indent=4)
+print json.dumps(lol, indent=4)
 with open('flare.json', 'w') as f:
-     json.dump(root, f)
+     json.dump(lol, f)
